4f70729b8ae05aaff7cfe605e0289b2eb2327031
YuWeiCong <769398477@qq.com>
2015-08-07 13:15:29 +0800

    修改旋转按钮样式

--- GeometryTool/GeometryTool/Model/ResizeRotateChrome.cs
+++ GeometryTool/GeometryTool/Model/ResizeRotateChrome.cs
@@ -12,5 +12,9 @@ namespace GeometryTool
         {
             FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(ResizeRotateChrome), new FrameworkPropertyMetadata(typeof(ResizeRotateChrome)));
         }
+        public ResizeRotateChrome()
+        {
+            int i=0;
+        }
     }
 }
